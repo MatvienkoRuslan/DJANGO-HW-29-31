7759c0238a0282550f3e92119f8d6c98776e09a8
Ruslan <kppmatvienko@gmail.com>
2023-02-13 20:07:17 +0200

    delete_another

--- task/forms.py
+++ task/forms.py
@@ -8,16 +8,5 @@ class TaskForm(forms.Form):
     completed = forms.BooleanField(required=False, initial=True, label='Completed')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    category = forms.ChoiceField(choices=[
-        ('home', 'Home'),
-        ('work', 'Work'),
-        ('personal', 'Personal'),
-        ('health', 'Health'),
-        ('finance', 'Finance'),
-        ('education', 'Education'),
-        ('leisure', 'Leisure'),
-        ('other', 'Other')
-    ], label='Category')
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label='Category')
 
-    def save(self):
-        pass
--- task/urls.py
+++ task/urls.py
@@ -4,5 +4,6 @@ from task import views
 
 urlpatterns = [
     path('task_all/', views.task_all, name='task_all'),
-    path('task_add/', views.task_add, name='task_all'),
+    path('task_add/', views.task_add, name='task_add'),
+    path('category/<int:cat_id>/', views.show_category, name='category'),
 ]
